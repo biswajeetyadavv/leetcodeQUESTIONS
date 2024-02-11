c1fd79931c912e18fae1eb3d8ede59d339e3aab3
Biswajeet yadav <biswajeetyadavofficial@gmail.com>
2024-02-12 01:39:27 +0530

    Add files via upload

---
+++ LEETCODE/src/Q1.java
@@ -0,0 +1,17 @@
+public class Q1 {
+    //        Q1. https:https//leetcode.com/problems/divide-array-into-arrays-with-max-difference/description/?envType=daily-question&envId=2024-02-01
+//        You are given an integer array nums of size n and a positive integer k.
+//
+//                Divide the array into one or more arrays of size 3 satisfying the following conditions:
+//
+//        Each element of nums should be in exactly one array.
+//        The difference between any two elements in one array is less than or equal to k.
+//        Return a 2D array containing all the arrays. If it is impossible to satisfy the conditions, return an empty array.And if there are
+//        multiple answers, return any of them.
+    public static void main(String[] args){
+        
+
+
+
+    }
+}
