3ee9d2cb6c54ff3b6dd6ac9bb5dd52312f9c86f0
Biswajeet yadav <biswajeetyadavofficial@gmail.com>
2024-02-13 13:49:09 +0530

    updated with ans of Q1.java

--- LEETCODE/src/Q1.java
+++ LEETCODE/src/Q1.java
@@ -1,4 +1,5 @@
 public class Q1 {
+   Q1. https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/?envType=daily-question&envId=2024-02-01
     //        Q1. https:https//leetcode.com/problems/divide-array-into-arrays-with-max-difference/description/?envType=daily-question&envId=2024-02-01
 //        You are given an integer array nums of size n and a positive integer k.
 //
@@ -8,9 +9,28 @@ public class Q1 {
 //        The difference between any two elements in one array is less than or equal to k.
 //        Return a 2D array containing all the arrays. If it is impossible to satisfy the conditions, return an empty array.And if there are
 //        multiple answers, return any of them.
-    public static void main(String[] args){
-        
 
+ public int[][] divideArray(int[] nums, int k) {
+        int arr[][] = new int[nums.length/3][3];
+        int line = 0;
+//        Arrays.sort(nums);
+        for(int i = 0;i<nums.length;i+=3){
+            int first = nums[i];
+            int sec = nums[i+1];
+            int third = nums[i+2];
+            if(third-first <= k){
+                arr[line][0] = first;
+                arr[line][1] = sec;
+                arr[line][2] = third;
+                line++;
+
+            }
+            else{
+                return new int[0][0];
+            }
+
+        }
+        return arr;
 
 
     }
